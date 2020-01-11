d197303168370d832b76007b747387a9ce5a06e8
Vadim Rutkovsky <vrutkovs@redhat.com>
2020-01-12 00:01:48 +0100

    Show infra failure

---
+++ src/components/JobRunRCARow.vue
@@ -0,0 +1,19 @@
+<template>
+  <b-tr variant="danger">
+    <b-td>{{ infra_failure }}</b-td>
+    <b-td
+      v-for="n in this.total"
+      :key="n">{{ n == index+1 ? "x" : "&nbsp;"}}</b-td>
+  </b-tr>
+</template>
+<script>
+export default {
+  name: 'JobRunRCARow',
+  props: {
+    infra_failure: String,
+    index: Number,
+    total: Number,
+    success: Boolean
+  }
+}
+</script>
--- src/pages/JobDetails.vue
+++ src/pages/JobDetails.vue
@@ -5,24 +5,38 @@
     </section>
 
     <section v-else>
-      <div v-if="loading">Loading...</div>
+      <div v-if="loading">...</div>
 
-      <div v-else>
-        <b-button-group>
-          <JobRunButton
-            v-for="(jb, idx) in job_info"
+      <b-table-simple v-else>
+        <b-thead>
+          <b-tr>
+            <b-th>Root Cause</b-th>
+            <JobRunButton
+              v-for="(jb, idx) in job_info"
+              :key="idx"
+              :runid="jb.id"
+              :jobid="jobid"
+              :success="jb.success"
+            />
+          </b-tr>
+        </b-thead>
+        <b-tbody>
+          <JobRunRCARow
+            v-for="(err, idx) in infraErrors"
             :key="idx"
-            :runid="jb.id"
-            :jobid="jobid"
-            :success="jb.success"
-          />
-        </b-button-group>
-      </div>
+            :index="err.idx"
+            :total="job_info.length"
+            :infra_failure="err.infra_failure"
+            success=false
+           />
+        </b-tbody>
+      </b-table-simple>
     </section>
   </div>
 </template>
 <script>
 import JobRunButton from '../components/JobRunButton.vue'
+import JobRunRCARow from '../components/JobRunRCARow.vue'
 
 export default {
   name: 'JobDetails',
@@ -30,6 +44,18 @@ export default {
     jobid() {
       return this.$route.params.jobid
     },
+    infraErrors() {
+      var result = []
+      this.job_info.map(function(v, idx){
+        if (v.success == false) {
+          result.push({
+            idx: idx,
+            infra_failure: v.infra_failure
+          })
+        }
+      })
+      return result
+    }
   },
   data(){
     return {
@@ -48,7 +74,7 @@ export default {
             .get(url)
             .then(response => {
               this.job_info.push(response.data)
-          })
+            })
         }, this)
       })
       .catch(error => {
@@ -61,7 +87,8 @@ export default {
       })
   },
   components: {
-    JobRunButton
+    JobRunButton,
+    JobRunRCARow
   }
 }
 </script>
