38f1e31e0f3d0a8cd6897219fe3f02bf5cc531a8
Vadim Rutkovsky <vrutkovs@redhat.com>
2020-01-11 22:45:48 +0100

    Fetch RCA for test runs

--- src/components/JobRunButton.vue
+++ src/components/JobRunButton.vue
@@ -1,16 +1,56 @@
 <template>
-  <b-button
-    :variant="success ? 'success' : 'danger'"
-    :to="`/job/${jobid}/${runid}`"
-  >{{ runid }}</b-button>
+  <section v-if="errored">
+    <p>Loading error</p>
+  </section>
+
+  <section v-else>
+    <div v-if="loading">Loading...</div>
+    <div v-else>
+      <b-button
+        :variant="success ? 'success' : 'danger'"
+        :to="`/job/${jobid}/${runid}`"
+      >{{ runid }}</b-button>
+    </div>
+  </section>
 </template>
 <script>
 export default {
   name: 'JobRunButton',
   props: {
     runid: Number,
-    jobid: Number,
+    jobid: String,
     success: Boolean
+  },
+  data(){
+    return {
+      rca: null,
+      failed_tests: [],
+      loading: true,
+      errored: false
+    }
+  },
+  mounted() {
+    this.axios
+      .get('http://localhost:3000/job/'+this.jobid+'/'+this.runid+'/rca')
+      .then(response => {
+        if (response.data.rca != null) {
+          this.rca = response.data.rca.get(0)
+        }
+        if (response.data.failed_tests == null) {
+          return
+        }
+        response.data.failed_tests.forEach(v => {
+          this.failed_tests.push(v)
+        }, this)
+      })
+      .catch(error => {
+        /* eslint no-console: ["error", { allow: ["warn", "error"] }] */
+        console.warn(error)
+        this.errored = true
+      })
+      .finally(() => {
+        this.loading = false
+      })
   }
 }
 </script>
